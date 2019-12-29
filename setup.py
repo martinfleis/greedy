7c9e3acb6908e084c470a7c3d4c6e15919fea9d1
Martin Fleischmann <martin@martinfleischmann.net>
2019-12-29 21:35:06 +0000

    unpin requirements

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=["geopandas", "networkx>=2.3", "libpysal<4.2.0"],
+    install_requires=["geopandas", "networkx", "libpysal"],
 )
